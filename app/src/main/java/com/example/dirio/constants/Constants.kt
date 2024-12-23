1615670506642a4295c2240ca1a7c4163df6bc02
vane <geovane08almeida@gmail.com>
2024-12-22 23:46:28 -0300

    error dateTime fixed

--- app/src/main/java/com/example/dirio/constants/Constants.kt
+++ app/src/main/java/com/example/dirio/constants/Constants.kt
@@ -3,8 +3,8 @@ package com.example.dirio.constants
 class Constants private constructor() {
 
      object DateOrDateTime{
-        const val date: String = "dd/MM/yyyy"
-        const val dateTime: String = "dd/MM/yyyy HH:mm"
+        const val date: String = "dd-MM-yyyy"
+        const val dateTime: String = "dd-MM-yyyy HH:mm"
     }
 
 }
